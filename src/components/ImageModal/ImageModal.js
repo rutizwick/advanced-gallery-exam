dcdaa1bc8e401cf7d88db368981716d75b3f1af8
Ruti Zwick <rutizwick@gmail.com>
2021-01-17 12:05:37 +0200

    Modal (enlarge photos) finished

--- src/components/ImageModal/ImageModal.js
+++ src/components/ImageModal/ImageModal.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import Modal from 'react-modal';
-import './ImageModal.css';
+import React from "react";
+import Modal from "react-modal";
+import "./ImageModal.css";
 
 export default class ImageModal extends React.Component {
   componentWillMount() {
-    Modal.setAppElement('body');
+    Modal.setAppElement("body");
+    console.log(this.props.imageData);
   }
 
   render() {
     const { isOpen, imageData, handleCloseModal, imgUrl } = this.props;
 
     return (
-      <Modal className='image-modal' isOpen={isOpen}>
-        <div>
-          <h2>{imageData.title}</h2>
-          <img alt={imageData.title} className='image-in-modal' src={imgUrl} />
-          <button onClick={handleCloseModal}>close Modal</button>
-        </div>
-      </Modal>
+          <Modal 
+          onRequestClose={handleCloseModal}
+          className="image-modal" 
+          isOpen={isOpen}>
+            <span className="close-button" onClick={handleCloseModal}>
+              &times;
+            </span>
+            <img
+              alt={imageData.title}
+              className="image-in-modal"
+              src={imgUrl}
+            />
+          </Modal>
     );
   }
 }
