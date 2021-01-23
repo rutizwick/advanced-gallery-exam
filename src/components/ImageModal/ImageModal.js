5027cf0bdfb85ff20d7883979558b67576761206
Ruti Zwick <rutizwick@gmail.com>
2021-01-23 09:58:52 +0200

    download styled

--- src/components/ImageModal/ImageModal.js
+++ src/components/ImageModal/ImageModal.js
@@ -10,43 +10,45 @@ export default class ImageModal extends React.Component {
   download(e) {
     e.preventDefault();
     fetch(e.target.href, {
-      method: "GET",
-      headers: {},
+      method: 'GET',
+      headers: {}
     })
       .then((response) => {
         response.arrayBuffer().then(function (buffer) {
           const url = window.URL.createObjectURL(new Blob([buffer]));
-          const link = document.createElement("a");
+          const link = document.createElement('a');
           link.href = url;
-          link.setAttribute("download", `img.png`); //or any other extension
+          link.setAttribute('download', 'img.png');
           document.body.appendChild(link);
           link.click();
         });
       })
       .catch((err) => {
-        console.log(err);
+        return err;
       });
   }
-  
+
   render() {
-    
     const { isOpen, imageData, handleCloseModal, imgUrl } = this.props;
 
     return (
-          <Modal
-          onRequestClose={handleCloseModal}
-          className='image-modal'
-          isOpen={isOpen}>
-            <span className='close-button' onClick={handleCloseModal}>
-              &times;
-            </span>
-            <a onClick={this.download} download href={imgUrl}>Download</a>
-            <img
-              alt={imageData.title}
-              className='image-in-modal'
-              src={imgUrl}
-            />
-          </Modal>
+      <Modal
+        onRequestClose={handleCloseModal}
+        className='image-modal'
+        isOpen={isOpen}
+      >
+        <span className='modal-button close-button' onClick={handleCloseModal}>
+          &times;
+        </span>
+        <a
+        className="modal-button download-button"
+        onClick={this.download}
+        download
+        href={imgUrl}>
+          📥
+        </a>
+        <img alt={imageData.title} className='image-in-modal' src={imgUrl} />
+      </Modal>
     );
   }
 }
